39d6a7582a1953fdb8fa3cfd193a66fadebfaa0e
glasses-and-hat <rahul.nagendra@live.com>
2026-04-10 10:11:07 -0500

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+
+        HashMap<Integer, Boolean> numCheck = new HashMap<>();
+
+        for(int num: nums) {
+            boolean check = numCheck.getOrDefault(num, false);
+            if(check) {
+                return true;
+            }
+            numCheck.put(num, true);
+        }
+        return false;
+    }
+}
