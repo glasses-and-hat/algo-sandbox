2dcd8751272f589e1e072dd7901e0cab87cc3f8f
glasses-and-hat <rahul.nagendra@live.com>
2026-04-11 14:43:31 -0500

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String, List<String>> mapOfAnagrams = new HashMap<>();
+
+        for(String str: strs) {
+            char[] strChars = str.toCharArray();
+            Arrays.sort(strChars);
+            String sortedChars = new String(strChars);
+
+            List<String> anagrams = mapOfAnagrams.getOrDefault(sortedChars, new ArrayList<String>());
+            anagrams.add(str);
+            mapOfAnagrams.put(sortedChars, anagrams);
+        }
+
+        List<List<String>> result = new ArrayList<>();
+
+        for(List<String> list: mapOfAnagrams.values()) {
+            result.add(list);
+        }
+        return result;
+        
+    }
+}
