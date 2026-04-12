30393f406bbedd5f6efb674f69321f41be5c7058
Rahul Yellapragada <Yellapragada.Tech@gmail.com>
2026-04-11 23:45:47 -0500

    Fixing import

--- Data Structures & Algorithms/is-anagram/submission-0.java
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
 
--- Data Structures & Algorithms/two-integer-sum/submission-0.java
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -1,10 +1,10 @@
+import java.util.*;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
         // num | index
         HashMap<Integer, Integer> numIndex = new HashMap<>();
-
-        int[] result = new int[2];
 
         for(int i = 0; i< nums.length; i++) {
             int diff = target - nums[i];
