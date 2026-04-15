c4402413f1ec002792accd6ea3a720ad4c130b09
glasses-and-hat <rahul.nagendra@live.com>
2026-04-14 22:27:38 -0500

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> seen = new HashSet<>();
+        for(int r = 0; r < 9; r++) {
+            for(int c = 0; c<9; c++) {
+                if(board[r][c] == '.') continue;
+                // int num = Integer.parseInt(""+board[r][c]);
+                int num = board[r][c] - '0';
+                if(num > 9 || num <= 0) return false;
+                String rowVal = num + "_in_row_" + r;
+                String colVal = num + "_in_col_" +c;
+                String boxVal = num + "_in_box_" + r/3 + "_" + c/3;
+                if(!seen.add(rowVal) | 
+                    !seen.add(colVal) |
+                    !seen.add(boxVal)) {
+                        return false;
+                    }
+            }
+        }
+        return true;
+    }
+}
