09971da8335ffa0ed4c72097c1481cd4d7edcf56
glasses-and-hat <rahul.nagendra@live.com>
2026-05-11 10:26:56 -0500

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * Using Floyd’s Fast & Slow Pointer technique:
+     *
+     * The slow pointer moves one step at a time.
+     * The fast pointer moves two steps at a time.
+     * If there’s a cycle, they will eventually meet.
+     **/
+    public int findDuplicate(int[] nums) {
+        int slow = 0, fast = 0;
+
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        int slow2 = 0;
+        while (true) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+            if (slow == slow2) {
+                return slow2;
+            }
+        }
+    }
+}
