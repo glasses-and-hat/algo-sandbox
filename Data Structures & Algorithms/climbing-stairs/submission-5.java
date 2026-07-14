f0b654e2db327c189f585e1cbc77e961ecb69e06
glasses-and-hat <rahul.nagendra@live.com>
2026-07-13 23:44:13 -0500

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        int a = 1, b = 1;
+
+        for (int i = 0; i < n - 1; i++) {
+            int t = a;
+            a = a + b;
+            b = t;
+        }
+        return a;
+    }
+}
